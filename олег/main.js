91cce649126e1a73c8da31af58054f066dc5d76c
itschoolgomother <itschoolgomother@gmail.com>
2020-01-15 13:52:02 +0200

    15.1.2020

---
+++ олег/main.js
@@ -0,0 +1,11 @@
+
+
+var test = prompt("курс","8800")
+var usd = 24;
+var cfh = 24.69;
+var huf = 0.08;
+var eur = 26.67;
+alert(Math.round(test/usd)+ "usd");
+alert(Math.round(test/cfh)+ "cfh");
+alert(Math.round(test/huf)+ "huf");
+alert(Math.round(test/eur)+ "eur");
