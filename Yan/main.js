ebc851286475565768c94e12f34f875c64f38cbc
itschoolgomother <itschoolgomother@gmail.com>
2020-01-18 15:07:17 +0200

    18.1.2020

--- Yan/main.js
+++ Yan/main.js
@@ -2,13 +2,20 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
-
-
 canvas.style.backgroundColor = "red";
 
-ctx.fillStyle = "indigo"
-ctx.fillRect(100,100,30,30);
 
 
-ss
-ctx.strokeRect(200,200,50,50);
+
+for (var i = 0; i<100;i++){ 
+var randomX = Math.random() * innerWidth;
+var randomY = Math.random() * innerHeight;	
+var random = Math.random() * 255;
+ctx.strokeStyle='rgb('+random+','+random+','+random+')';
+ctx.strokeRect(randomX , randomY,random,random)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+
+
+
+
+}
+
