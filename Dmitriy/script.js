1c34385dee84642295401680ca30121949ec1d21
itschoolgomother <itschoolgomother@gmail.com>
2020-01-19 19:30:53 +0200

    19.1.2020

--- Dmitriy/script.js
+++ Dmitriy/script.js
@@ -1,11 +1,13 @@
-var test = document.getElementById("box");
-var colors = ["red","green","blue","indigo","gold"];
+// var test = document.getElementById("box");
+// var colors = ["red","green","blue","indigo","gold"];
 
 
 
-test.style.backgroundColor = colors[4];
-test.style.width = "100px"
-test.style.height = "100px"
+// test.style.backgroundColor = colors[4];
+// test.style.width = "100px"
+// test.style.height = "100px"
 
-alert("hi world");
-console.log("123123123123132");
+// alert("hi world");
+// console.log("123123123123132");
+alert(String.fromCharCode(97))
+alert("Dima".charCodeAr(0))
---
+++ Svitlana/main.js
@@ -0,0 +1,9 @@
+var color = prompt ("Виберыть корыр","pink")
+var sais = prompt("Виберыть число" ,"78");
+var box = document.getElementById("box");
+var age = prompt ("Скыльки вам рокыв","10");
+var name = prompt ("Ваше Имя","Svitlana");
+alert("Hi " + name +" " +age)
+box.style.width = sais+"px";
+box.style.height = sais+"px";
+box.style.backgroundColor = color;
