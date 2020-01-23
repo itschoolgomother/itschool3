ec22486ca032647b66ecc6e8d98840ed23cd3376
itschoolgomother <itschoolgomother@gmail.com>
2020-01-23 19:24:24 +0200

    23.1.2020

---
+++ dasha/mein.js
@@ -0,0 +1,4 @@
+var x = prompt("vashe chislo?","7")
+var y = prompt("vashe chislo?","3")
+alert(x*y)
+
--- олег/main.js
+++ олег/main.js
@@ -1,11 +1,17 @@
 
 
-var test = prompt("курс","8800")
-var usd = 24;
-var cfh = 24.69;
-var huf = 0.08;
-var eur = 26.67;
-alert(Math.round(test/usd)+ "usd");
-alert(Math.round(test/cfh)+ "cfh");
-alert(Math.round(test/huf)+ "huf");
-alert(Math.round(test/eur)+ "eur");
+// var test = prompt("курс","8800")
+// var usd = 24;
+// var cfh = 24.69;
+// var huf = 0.08;
+// var eur = 26.67;
+// alert(Math.round(test/usd)+ "usd");
+// alert(Math.round(test/cfh)+ "cfh");
+// alert(Math.round(test/huf)+ "huf");
+// alert(Math.round(test/eur)+ "eur");
+var box  = document.getElementById("box")
+var size  = prompt("size","100")
+var color= prompt("blue");
+box.style.width = size+"px";
+box.style.height = size+"px";
+box.style.backgroundColor = color;
