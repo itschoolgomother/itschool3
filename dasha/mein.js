4a9fdc8e433b61b3607602970be58d4cee399b5c
itschoolgomother <itschoolgomother@gmail.com>
2020-01-30 19:29:36 +0200

    30.1.2020

--- dasha/mein.js
+++ dasha/mein.js
@@ -1,4 +1,25 @@
-var x = prompt("vashe chislo?","7")
-var y = prompt("vashe chislo?","3")
-alert(x*y)
+var say = prompt("Ваш пароль","passworld");
+var time = new Date();
+var mypas = ""+time.getMinutes()
 
+
+if(say == mypas){
+	alert("Привет, Dasha")
+code()
+}else{
+	alert("Пока")
+}
+
+
+
+
+function code(){
+var usd = 24.5
+var x = prompt("Ваше число?","10")
+
+var resutGrn = usd*x
+var resutUsd = x/usd
+
+alert(resutGrn.toFixed(2) + "грн") 
+alert(resutUsd.toFixed(2) + "$")
+}
