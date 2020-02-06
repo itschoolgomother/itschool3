d1a353a185f5d9eafa2bfcb472cec7033e30f054
itschoolgomother <itschoolgomother@gmail.com>
2020-02-06 19:45:27 +0200

    6.2.2020

---
+++ dasha/rondom/mein.js
@@ -0,0 +1,16 @@
+var rand = Math.ceil(Math.random()*10)
+var num = prompt("Ot 0 do 10","10");
+
+if(rand == num){
+    alert("Угадала")
+}else{
+     alert("Не угадала"+rand)
+}
+
+var red = Math.random()*255
+var green = Math.random()*255
+var blue = Math. random()*255
+
+document.body.style.backgroundColor = "rgb("+red+","+green+","+blue+")"
+
+
